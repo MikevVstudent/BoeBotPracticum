a5d24dac78ebb9c58c956212e7b1b85f883ef9f0
Mike <mikeverseveld@outlook.com>
2019-12-12 12:25:17 +0100

    opdracht 7

--- BoeBotProj/src/Week2/Week2_7b.java
+++ BoeBotProj/src/Week2/Week2_7b.java
@@ -1,22 +1,15 @@
 package Week2;
 
 import TI.BoeBot;
-import TI.Servo;
 
-public class Week2_6c {
-    private static Servo s1 = new Servo(12);
-    private static Servo s2 = new Servo(13);
-
+public class Week2_7b {
     public static void main (String[] args){
-        GaNaarSnelheid gaNaarSnelheid = new GaNaarSnelheid();
-        NoodRem noodRem = new NoodRem();
-        Draaien draaien = new Draaien();
-
-        while (true){
-            gaNaarSnelheid.gaNaarSnelheid(200);
-            noodRem.noodRem();
-            draaien.draaien(200);
-            BoeBot.wait(2000);
+        int i = 1000;
+        while (i<2000){
+            i++;
+            BoeBot.freqOut(0, i , 1);
+            BoeBot.wait(1);
+            if(i == 2000) i = 1000;
         }
     }
 }
